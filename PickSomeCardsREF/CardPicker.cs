ae18b4a52bef4667720a45f0bfe27f1fb62a8479
Chintan Bharateeya <bhatschintan@gmail.com>
2023-08-14 14:30:33 +0530

    Add project files.

---
+++ PickSomeCardsREF/CardPicker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCardsREF
+{
+    internal class CardPicker
+    {
+        public static List<string> deck = new List<string>();
+        public static List<string> SetUpCards()
+        {
+            List<string> suits = new List<string>() { "Spade", "Club", "Heart", "Diamond" };
+            List<string> ranks = new List<string>() { "2", "3", "4", "5","6", "7", "8",
+                                                      "9", "10", "Jack","Queen","King",
+                                                                                "Ace" };
+            
+            foreach (string suit in suits)
+            {
+                foreach (string rank in ranks)
+                {
+                    deck.Add($"{rank} of {suit}");
+                }
+            }
+            return deck;
+        }
+        int cards = 0;
+        public static List<string> RandomCards(int numberOfCards)
+        {
+            Random random = new Random();
+            List<string> pickedCards = new List<string>();
+            for (int i=0; i<numberOfCards; i++)
+            {
+                int randomNumber = random.Next(deck.Count);
+                pickedCards.Add (deck[randomNumber]);
+                deck.RemoveAt(randomNumber);
+
+
+            }
+            return pickedCards;
+        }
+        
+    }
+}
---
+++ PickSomeCardsREF/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PickRandomCardsREF;
+using System.Transactions;
+CardPicker.SetUpCards();
+Console.Write("Enter the number of Cards to Print");
+string line= Console.ReadLine();
+if(int.TryParse(line, out int numberOfCards))
+{
+    if (numberOfCards > CardPicker.deck.Count)
+    {
+        Console.WriteLine("The Entry Can't be larger then the Deck, Should be between 1-52");
+
+    }
+    foreach(string card in CardPicker.RandomCards(numberOfCards))
+    { Console.WriteLine(card); }
+
+}
+else
+{
+    Console.WriteLine("Enter A Valid Number");
+}
